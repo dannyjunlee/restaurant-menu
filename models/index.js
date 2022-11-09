d37f93183e31ec2fca5215ca1dcbd82f0907e7a2
Jax I <jaxpi85@gmail.com>
2022-11-09 13:37:58 -0500

    debugging index models

--- models/index.js
+++ models/index.js
@@ -4,15 +4,27 @@ const Review = require('./reviews');
 
 User.hasMany(Dish, {
   foreignKey: 'User_id',
-    onDelete: "CASCADE"
 });
 
 Dish.hasMany(Review, {
     foreignKey: 'dish_id',
-      onDelete: "CASCADE"
+});
+
+User.hasMany(Review, {
+    foreignKey: 'User_id',
 });
 
 Review.belongsTo(Dish, {
+    foreignKey: 'dish_id',
+    onDelete: "CASCADE"
+});
+
+Review.belongsTo(User, {
+    foreignKey: 'User_id',
+    onDelete: "CASCADE"
+});
+
+Dish.belongsTo(User, {
     foreignKey: 'dish_id',
     onDelete: "CASCADE"
 });

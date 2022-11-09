ac3b6b92d432120cbe8dbce214eaf9b5072abe56
Jax I <jaxpi85@gmail.com>
2022-11-09 12:56:34 -0500

    populated review data file

---
+++ models/reviewData.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Review extends Model {}
+
+Review.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    comments: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    score: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'user',
+            key: 'id',
+          },
+      },
+    dish_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'menu',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'menu',
+  }
+);
+
+module.exports = Review;

d82b944e3767d40a0a6785dfd7d34a2ec6562071
Jax I <jaxpi85@gmail.com>
2022-11-09 13:51:28 -0500

    created db name in schema.sql

--- models/index.js
+++ models/index.js
@@ -1,32 +1,29 @@
-// const User = require('./users');
-// const Dish = require('./dishes');
-// const Review = require('./reviews');
+const User = require('./users');
+const Dish = require('./dishes');
+const Review = require('./reviews');
 
-// // User.hasMany(Dish, {
-// //   foreignKey: 'User_id',
-// // });
+User.hasMany(Dish, {
+  foreignKey: 'user_id',
+});
 
-// // Dish.hasMany(Review, {
-// //     foreignKey: 'dish_id',
-// // });
+Dish.hasMany(Review, {
+    foreignKey: 'dish_id',
+});
 
-// // User.hasMany(Review, {
-// //     foreignKey: 'User_id',
-// // });
+User.hasMany(Review, {
+    foreignKey: 'user_id',
+});
 
-// // Review.belongsTo(Dish, {
-// //     foreignKey: 'dish_id',
-// //     onDelete: "CASCADE"
-// // });
+Review.belongsTo(Dish, {
+    foreignKey: 'dish_id',
+});
 
-// // Review.belongsTo(User, {
-// //     foreignKey: 'User_id',
-// //     onDelete: "CASCADE"
-// // });
+Review.belongsTo(User, {
+    foreignKey: 'user_id',
+});
 
-// // Dish.belongsTo(User, {
-// //     foreignKey: 'dish_id',
-// //     onDelete: "CASCADE"
-// // });
+Dish.belongsTo(User, {
+    foreignKey: 'user_id',
+});
 
-// module.exports = { User, Dish, Review };
+module.exports = { User, Dish, Review };

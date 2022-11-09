fa1b905af5d91dbdacf3a9056a5155fff0cfa13f
Danny <dl667@cornell.edu>
2022-11-09 13:53:18 -0500

    added to home routes the get routes and login route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,16 +1,20 @@
 const router = require('express').Router();
-const { menuData } = require('../models');
+const { Dish, Review, User } = require('../models');
 
+
+// GET route for homepage
 router.get('/', async (req, res) => {
     try {
-        const menu = await menuData.findAll();
+        const dishData = await Dish.findAll({
+            include: [{ model: Review }]
+        });
 
-        const menus = menu.map((menuItem) =>
-            menuItem.get({ plain: true })
+        const dishes = dishData.map((dish) =>
+            dish.get({ plain: true })
         );
 
         res.render('homepage', {
-            menus,
+            dishes,
             loggedIn: req.session.loggedIn
         });
     } catch (err) {
@@ -18,6 +22,31 @@ router.get('/', async (req, res) => {
     };
 });
 
+// GET route for one dish
 router.get('/:id', async (req, res) => {
+    try {
+        const dishData = await Dish.findByPk(req.params.id, {
+            include: [{ model: Review }]
+        });
 
-})
+        const dishes = dishData.get({ plain: true });
+
+        res.render('dish', {
+            dishes,
+            loggedIn: req.session.loggedIn
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    };
+});
+
+// Login route
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
+
+module.exports = router;

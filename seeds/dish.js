49ebae2a37267378b6dbe78ee486115f601718b3
Jax I <jaxpi85@gmail.com>
2022-11-09 17:01:19 -0500

    added more data to dish table

--- seeds/dish.js
+++ seeds/dish.js
@@ -3,38 +3,38 @@ const { Dish } = require('../models');
 const dishdata = [
   {
     dish_type: 'Appetizer',
-    dish_name: 'Sample Dish 1',
-    dish_description: 'Sample Description 1',
+    dish_name: 'Pumpkaboo Porridge',
+    dish_description: 'Sweet and savory pumpkin porridge with a dash of cinnamon and nutmeg - so good you will be Gulpin it down',
     dish_price: 5.00
   },
   {
     dish_type: 'Appetizer',
-    dish_name: 'Sample Dish 2',
-    dish_description: 'Sample Description 2',
+    dish_name: 'Chesnaught Hummus',
+    dish_description: 'Chestnut hummus served with herbed crackers - (gluten-free crackers!)',
     dish_price: 5.00
   },
   {
     dish_type: 'Appetizer',
-    dish_name: 'Sample Dish 3',
-    dish_description: 'Sample Description 3',
+    dish_name: 'Stuffed Mushrooms',
+    dish_description: 'A very Shroomish dish when you need a Foongus fix',
     dish_price: 5.00
   },
   {
     dish_type: 'Appetizer',
-    dish_name: 'Sample Dish 4',
-    dish_description: 'Sample Description 4',
+    dish_name: 'Garden Salad',
+    dish_description: 'Fresh garden vegetables and Bellsprouts on a bed of spinach and iceberg lettuce Leafeons',
     dish_price: 5.00
   },
   {
     dish_type: 'Appetizer',
-    dish_name: 'Sample Dish 5',
-    dish_description: 'Sample Description 5',
+    dish_name: 'Arti-Machoke Hearts',
+    dish_description: 'Caramelized maple syrup gives these artichoke hearts a Blastoise of flavor',
     dish_price: 5.00
   },
   {
     dish_type: 'Appetizer',
-    dish_name: 'Sample Dish 6',
-    dish_description: 'Sample Description 6',
+    dish_name: 'Snap Peas w/Dipping Sauce',
+    dish_description: 'Farm-fresh snap peas in their Metapods and a Sunflora seed butter-based creamy sauce',
     dish_price: 5.00
   },
   {
@@ -111,14 +111,14 @@ const dishdata = [
   },
   {
     dish_type: 'Dessert',
-    dish_name: 'Sample Dish 4',
-    dish_description: 'Sample Description 4',
+    dish_name: 'Vanilla Ice Cream Cup',
+    dish_description: 'Comes in 3 sizes - Vanillite (S), Vanillish (M), and Vanilluxe(L)',
     dish_price: 10.00
   },
   {
     dish_type: 'Dessert',
-    dish_name: 'Sample Dish 4',
-    dish_description: 'Sample Description 4',
+    dish_name: 'Jiggly-Cream-Puffs',
+    dish_description: 'Sweet cream puffs filled with strawberry Jellicent',
     dish_price: 10.00
   },
   {
@@ -142,7 +142,7 @@ const dishdata = [
   {
     dish_type: 'Drink',
     dish_name: 'Flapple Juice',
-    dish_description: 'Sweet and crisp apple slices are added for a Blastoise of fresh Applin flavor',
+    dish_description: 'Sweet and crisp apple slices are added for a fresh Applin flavor',
     dish_price: 2.00
   },
 ];

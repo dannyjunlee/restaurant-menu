bac07bce58445221543d2c5a2e363be18086a266
Jax I <jaxpi85@gmail.com>
2022-11-09 16:16:44 -0500

    created placeholders for dish names

--- seeds/dish.js
+++ seeds/dish.js
@@ -14,6 +14,84 @@ const dishdata = [
     dish_price: 5.00
   },
   {
+    dish_type: 'Appetizer',
+    dish_name: 'Sample Dish 3',
+    dish_description: 'Sample Description 3',
+    dish_price: 5.00
+  },
+  {
+    dish_type: 'Appetizer',
+    dish_name: 'Sample Dish 4',
+    dish_description: 'Sample Description 4',
+    dish_price: 5.00
+  },
+  {
+    dish_type: 'Appetizer',
+    dish_name: 'Sample Dish 5',
+    dish_description: 'Sample Description 5',
+    dish_price: 5.00
+  },
+  {
+    dish_type: 'Appetizer',
+    dish_name: 'Sample Dish 6',
+    dish_description: 'Sample Description 6',
+    dish_price: 5.00
+  },
+  {
+    dish_type: 'Entree',
+    dish_name: 'Sample Dish 3',
+    dish_description: 'Sample Description 3',
+    dish_price: 15.00
+  },
+  {
+    dish_type: 'Entree',
+    dish_name: 'Sample Dish 3',
+    dish_description: 'Sample Description 3',
+    dish_price: 15.00
+  },
+  {
+    dish_type: 'Entree',
+    dish_name: 'Sample Dish 3',
+    dish_description: 'Sample Description 3',
+    dish_price: 15.00
+  },
+  {
+    dish_type: 'Entree',
+    dish_name: 'Sample Dish 3',
+    dish_description: 'Sample Description 3',
+    dish_price: 15.00
+  },
+  {
+    dish_type: 'Entree',
+    dish_name: 'Sample Dish 3',
+    dish_description: 'Sample Description 3',
+    dish_price: 15.00
+  },
+  {
+    dish_type: 'Entree',
+    dish_name: 'Sample Dish 3',
+    dish_description: 'Sample Description 3',
+    dish_price: 15.00
+  },
+  {
+    dish_type: 'Entree',
+    dish_name: 'Sample Dish 3',
+    dish_description: 'Sample Description 3',
+    dish_price: 15.00
+  },
+  {
+    dish_type: 'Entree',
+    dish_name: 'Sample Dish 3',
+    dish_description: 'Sample Description 3',
+    dish_price: 15.00
+  },
+  {
+    dish_type: 'Entree',
+    dish_name: 'Sample Dish 3',
+    dish_description: 'Sample Description 3',
+    dish_price: 15.00
+  },
+  {
     dish_type: 'Entree',
     dish_name: 'Sample Dish 3',
     dish_description: 'Sample Description 3',
@@ -24,6 +102,48 @@ const dishdata = [
     dish_name: 'Sample Dish 4',
     dish_description: 'Sample Description 4',
     dish_price: 10.00
+  },
+  {
+    dish_type: 'Dessert',
+    dish_name: 'Sample Dish 4',
+    dish_description: 'Sample Description 4',
+    dish_price: 10.00
+  },
+  {
+    dish_type: 'Dessert',
+    dish_name: 'Sample Dish 4',
+    dish_description: 'Sample Description 4',
+    dish_price: 10.00
+  },
+  {
+    dish_type: 'Dessert',
+    dish_name: 'Sample Dish 4',
+    dish_description: 'Sample Description 4',
+    dish_price: 10.00
+  },
+  {
+    dish_type: 'Drink',
+    dish_name: 'Orange Bellossom Iced-Tea',
+    dish_description: 'Refreshing citrus tea with a Squirtle of orange syrup',
+    dish_price: 2.00
+  },
+  {
+    dish_type: 'Drink',
+    dish_name: 'Snorlax Tea',
+    dish_description: 'This tea might make you Drowzee with its relaxing chamomile, lavender, and valerian',
+    dish_price: 2.00
+  },
+  {
+    dish_type: 'Drink',
+    dish_name: 'Peppermint Sinistea',
+    dish_description: 'Cooling and anti-inflammatory peppermint is great for when you are Koffing and Weezing',
+    dish_price: 2.00
+  },
+  {
+    dish_type: 'Drink',
+    dish_name: 'Flapple Juice',
+    dish_description: 'Sweet and crisp apple slices are added for a Blastoise of fresh Applin flavor',
+    dish_price: 2.00
   },
 ];
 

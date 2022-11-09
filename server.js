4ae3edd66aa1a06e41031e98fa4d788a366bdb66
Danny <dl667@cornell.edu>
2022-11-09 12:33:32 -0500

    index file for controllers

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 
-const route = require('./controllers');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const helpers = require('./utils/helper');
 
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 const sess = {
-  secret: 'Shush secret secret',
+  secret: 'top secret hush hush',
   cookie: {},
   resave: false,
   saveUnitialized: true,

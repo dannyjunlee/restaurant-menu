dfe81f66008163315b0869d7468ab2a2a9be7e34
KC <kevinchen@kevins-air.myfiosgateway.com>
2022-11-09 13:43:56 -0500

    index seed update

--- seeds/index.js
+++ seeds/index.js
@@ -1,9 +1,11 @@
 const sequelize = require('../config/connection');
-const seedMenu = require('./menu')
+const seedMenu = require('./menu');
+const seedReview = require('./review');
 
 const seedAll = async () => {
     await sequelize.sync({ force: true});
     await seedMenu();
+    await seedReview();
     process.exit(0)
 };
 

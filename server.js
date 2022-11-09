241571f06e987c481b0f8cc0d2a63be2e009c500
KC <kevinchen@kevins-air.myfiosgateway.com>
2022-11-09 10:49:07 -0500

    route

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const sequelize = require('')
+const route = require('./controllers')
+const helpers = require('/utils/helpers')
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });

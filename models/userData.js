f8290cb6983cefa3d9b8b083de0acaf73711c9cc
Jax I <jaxpi85@gmail.com>
2022-11-09 13:01:51 -0500

    populated userdata with keys

--- models/userData.js
+++ models/userData.js
@@ -4,7 +4,38 @@ const sequelize = require('../config/connection');
 class User extends Model {}
 
 User.init(
-  
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    password: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    phone: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    address: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    favorites: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+  },
   {
     sequelize,
     freezeTableName: true,

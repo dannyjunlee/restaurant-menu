c586a03178f98d57ea52666da7ff492dca68414e
Goat <dpajbk@gmail.com>
2022-11-10 13:36:15 -0500

    added route for user to get all their own reviews

--- controllers/api/reviewRoutes.js
+++ controllers/api/reviewRoutes.js
@@ -39,6 +39,24 @@ router.get('/:dish_id', async (req, res) => {
 });
 
 
-// get all reviews by one user''
+// get all your own reviews
+
+router.get('/myreviews', async (req, res) => {
+    try {
+        const reviewData = await Review.findAll({
+            where: {
+                user_id: req.session.user_id
+            },
+        });
+        if (reviewData.length === 0) {
+            res.status(404).json({ message: "You haven't posted any reviews yet!" })
+            return
+        }
+        res.status(200).json(reviewData);
+        console.log(reviewData)
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;

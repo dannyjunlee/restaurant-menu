69b76547983ae3f0d54e80ac39278c4f7a84cffe
Goat <dpajbk@gmail.com>
2022-11-09 22:16:18 -0500

    added create user route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -15,5 +15,28 @@ router.get('/', async (req, res) => {
     res.json(userData);
 });
 
+// create new user
+router.post('/', async (req, res) => {
+    try {
+      const userData = await User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        phone: req.body.phone,
+        address: req.body.address,
+        // TODO: figure out credit card validation
+        // creditcard: req.body.creditcard,
+      });
+  
+      req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.loggedIn = true;
+  
+        res.status(200).json(userData);
+      });
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
 
 module.exports = router;

d0502af94954fca42a6a18d61b889c4bf2a450f6
Igor Korman <ikorman12@gmail.com>
2022-11-10 11:02:10 -0500

    added some minor changes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
-// const router = require('express').Router();
-// const dishRoutes = require('./dishRoutes');
-// const userRoutes = require('./userRoutes');
-// const reviewRoutes = require('./reviewRoutes');
+const router = require('express').Router();
+const dishRoutes = require('./dishRoutes');
+const userRoutes = require('./userRoutes');
+const reviewRoutes = require('./reviewRoutes');
 
-// router.use('/dishes', dishRoutes);
+router.use('/dishes', dishRoutes);
 // router.use('/users', userRoutes);
 // router.use('/reviews', reviewRoutes);
 
-// module.exports = router;
+module.exports = router;

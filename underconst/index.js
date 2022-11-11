67cfcb459f61ff37ab4961e0891b3666e690d28d
Jax I <jaxpi85@gmail.com>
2022-11-11 12:54:07 -0500

    created index.js for hide/pop-up functioning for sign-in modal

---
+++ underconst/index.js
@@ -0,0 +1,16 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}

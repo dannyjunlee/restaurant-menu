f4a55ef90b72f32ee32560ff2462e8826fd97f94
Jax I <jaxpi85@gmail.com>
2022-11-13 08:23:52 -0500

    added sign up button and new functionality to the action center modal - still needs work but I need help with it when someone has a chance. I will continue working on it when I have time

--- public/js/index.js
+++ public/js/index.js
@@ -7,14 +7,32 @@ var btn = document.getElementById("myBtn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
+// Function to show the modal when the sign in button is clicked
 function openForm() {
   document.getElementById("myForm").style.display = "inline";
 }
 
+// Function to hide the modal when the close button is clicked
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
 
-// function openCart() {
-//   document.getElementById("cartContainer").style.display = "block";
+// Function to hide the modal when the Login button is clicked and change the text in the sign in button to "Logged In"
+function signIn() {
+    document.getElementById("myForm").style.display = "none";
+    document.getElementById("openButton").innerHTML = "Logged In";
+    //this shows "Logged In" for a brief moment then goes back to "Sign In"
+    //also needs to be tied into session storage for user data and an if else statement so that if the user info is not valid it remains open and asks for valid info or to sign up instead
+}
+
+//This function below also needs work and integration with session storage, etc
+
+// function signUp() {
+//     //post new user with that username and password;
+//     document.getElementById("myForm").style.display = "none";
+//     if (loggedIn === true) {
+//         document.getElementById("openButton").innerHTML = "Logged In";
+//     } else {
+//         openForm();
+//     }
 // }

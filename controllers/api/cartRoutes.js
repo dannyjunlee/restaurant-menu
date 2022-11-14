9b44edc33af7fd410e7f00795a4f69a8195ca4ce
Igor Korman <ikorman12@gmail.com>
2022-11-14 15:05:56 -0500

    added cart routes, with integrated function for adding and subratcting objects

---
+++ controllers/api/cartRoutes.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    if (!req.session.cart) {
+        req.session.cart = {};
+    }
+
+    res.status(200).json(req.session.cart);
+})
+
+router.put('/', (req, res) => {
+    if (!req.session.cart) req.session.cart = {};
+    const cart = req.session.cart;
+    const { dishName, number, price } = req.body;
+    if (!cart[dishName]) {
+        cart[dishName] = {
+            number: 0,
+            price: price
+        };
+    }
+    cart[dishName].number += number;
+    req.session.cart = cart;
+    console.log(Object.entries(cart));
+    res.json(cart);
+});
+
+router.delete('/', (req, res) => {
+    req.session.cart = {};
+    res.status(200).json({ msg: "cleared cart" });
+})
+
+module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const dishRoutes = require('./dishRoutes');
 const userRoutes = require('./userRoutes');
 const reviewRoutes = require('./reviewRoutes');
+const cartRoutes = require('./cartRoutes');
 
 router.use('/dishes', dishRoutes);
 router.use('/users', userRoutes);
 router.use('/reviews', reviewRoutes);
+router.use('/cart', cartRoutes);
 
 module.exports = router;

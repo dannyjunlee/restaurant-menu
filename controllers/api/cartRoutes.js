e6889542b36300b721507c9e2cf69a48711c401e
Igor Korman <ikorman12@gmail.com>
2022-11-14 16:13:53 -0500

    added functions in cart.js, tried out foor lopps in cart routes

--- controllers/api/cartRoutes.js
+++ controllers/api/cartRoutes.js
@@ -11,9 +11,10 @@ router.get('/', (req, res) => {
 router.put('/', (req, res) => {
     if (!req.session.cart) req.session.cart = {};
     const cart = req.session.cart;
-    const { dishName, number, price } = req.body;
+    const { dishName: dishName, number: number, price: price } = req.body;
     if (!cart[dishName]) {
         cart[dishName] = {
+            dishName: dishName,
             number: 0,
             price: price
         };
--- public/js/cart.js
+++ public/js/cart.js
@@ -1,17 +1,33 @@
-const clearCartButton = document.getElementById("clearCart")
-clearCartButton.addEventListener("click", (event) => {
-    fetch('/cart/clear') 
-    window.location.href = '/cart'
-});
+// const clearCartButton = document.getElementById("clearCart")
+// clearCartButton.addEventListener("click", (event) => {
+//     fetch('/cart/clear') 
+//     window.location.href = '/cart'
+// });
+const cartCon = document.querySelector('.cartCon')
 
+async function renderCart() {
+    const response = await fetch('/api/cart', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    });
+    const cart = await response.json();
+    console.log(cart);
 
+    const results = Object.entries(cart);
+    console.log(results.length);
+    for (let i = 1; i < results.length; i++) {
+        console.log(results[i], 'from loop');
+        const p = document.createElement('p')
+        const a = document.createElement('a').setAttribute('href', "#");
+        const span = document.createElement('span');
+        a.textContent = results[i].dishName;
+        span.textContent = results[i].price;
+        p.append(a, span);
+        cartCon.append(p);
 
+    }
+};
 
-
-
-
-
-
-
-
-
+renderCart();

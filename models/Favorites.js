182913a25a6d13d5cf669fb28f234eebd57f8858
Goat <dpajbk@gmail.com>
2022-11-14 13:42:21 -0500

    primary key added

--- models/Favorites.js
+++ models/Favorites.js
@@ -3,3 +3,17 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 const User = require('./User');
 const Dish = require('./Dish');
+
+class Favorite extends Model {}
+
+Favorites.init(
+    {
+        // define columns
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        }
+    }
+)

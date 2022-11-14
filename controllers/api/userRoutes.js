9a54600cb47bf7bb96c9c42129925cbd8534c2bb
KC <kevinchen@kevins-air.myfiosgateway.com>
2022-11-14 12:50:33 -0500

    req.session

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -24,6 +24,7 @@ router.post('/', async (req, res) => {
         req.session.save(() => {
             req.session.user_id = userData.id;
             req.session.loggedIn = true;
+            req.session.cart = []
 
             res.status(200).json(userData);
         });
@@ -108,6 +109,7 @@ router.post('/login', async (req, res) => {
         req.session.save(() => {
             req.session.user_id = dbUserData.id;
             req.session.loggedIn = true;
+            req.session.cart = [];
             
 
             res

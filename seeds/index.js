2fc63f5ed697b40f3eac54112e2a8698de0aa9eb
Danny <dl667@cornell.edu>
2022-11-14 18:16:33 -0500

    sequelize literal

--- seeds/index.js
+++ seeds/index.js
@@ -5,6 +5,7 @@ const seedUsers = require('./userData');
 
 const seedAll = async () => {
     await sequelize.sync({ force: true });
+    await sequelize.literal('SET FOREIGN_KEY_CHECKS = 0; DROP TABLE dish IF EXISTS; DROP TABLE user IF EXISTS; DROP TABLE review IF EXISTS; SET FOREIGN_KEY_CHECKS = 1;');
     await seedDish();
     await seedUsers();
     await seedReview();

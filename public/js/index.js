d030b63d6a099fd99932688acb93cf7fe7f51ed5
Jax I <jaxpi85@gmail.com>
2022-11-14 15:07:29 -0500

    added favorite buttons for each dish and styled them

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,3 @@
-// Get the modal
-// const modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-// const btn = document.getElementById("myBtn");
-
 // Get the <span> element that closes the modal
 const span = document.getElementsByClassName("close")[0];
 
@@ -27,27 +21,6 @@ function openCart() {
 function closeCart() {
   document.getElementById("cartContainer").style.display = "none";
 }
-
-    //this shows "Logged In" for a brief moment then goes back to "Sign In"
-    //also needs to be tied into session storage for user data and an if else statement so that if the user info is not valid it remains open and asks for valid info or to sign up instead like
-    // if (loggedIn == true) {
-    //     document.getElementById("myForm").style.display = "none";
-    //     document.getElementById("openButton").innerHTML = "Logged In";
-    // } else {
-    //     openForm();
-    // };
-
-//This function below also needs work and integration with session storage, etc
-
-// function signUp() {
-//     //post new user with that username and password;
-//     document.getElementById("myForm").style.display = "none";
-//     if (loggedIn === true) {
-//         document.getElementById("openButton").innerHTML = "Logged In";
-//     } else {
-//         openForm();
-//     }
-// }
 
 
 // geolocation

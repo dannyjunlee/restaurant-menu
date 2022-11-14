8e5a840d1787e91d5b06cbf54d04811747d0dc38
Danny <dl667@cornell.edu>
2022-11-14 11:38:21 -0500

    implement withAuth

--- public/js/login.js
+++ public/js/login.js
@@ -13,7 +13,7 @@ const loginFormHandler = async (event) => {
         });
 
         if (response.ok) {
-            document.location.replace('/');
+            window.location.replace('/');
         } else {
             alert('Failed to log in.');
         }
@@ -36,7 +36,7 @@ const signupFormHandler = async (event) => {
         });
 
         if (response.ok) {
-            document.location.replace('/');
+            window.location.replace('/');
         } else {
             alert('Failed to sign up.');
         }
